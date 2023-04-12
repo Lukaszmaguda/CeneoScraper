d9d9970a59411c9fe2d540137b6d02a548ed90d6
Lukaszmaguda <rometzet@gmail.com>
2023-04-12 14:46:34 +0200

    2023-04-12 łm

--- scraper.py
+++ scraper.py
@@ -1,8 +1,34 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
-def get_cons(selector, ancestor):
-    return ancestor.opinion.select_one(selector).text.strip()
+def get_element(ancestor, selector=None, attribute = None, return_list=False):
+    try:
+        if return_list:
+            return [tag.text.strip() for tag in ancestor.select(selector)]
+        if not selector and attribute:
+            return ancestor[attribute]
+        if attribute:
+            return ancestor.select_one(selector)[attribute].strip()
+        return ancestor.opinion.select_one(selector).text.strip()
+    except (AttributeError, TypeError):
+        return None
+    
+selectors = {
+    "opinion_id": [None, "data-entry-id"],
+    "author": ["span.user-post__author-name"],
+    "recommendation": ["span.user-post__author-recomendation > em"],
+    "stars": ["span.user-post__score-count"],
+    "purchased": ["div.review-pz"],
+    "opinion_date": ["span.user-post__published > time:nth-child(1)","datetime"],
+    "purchase_date": ["span.user-post__published > time:nth-child(2)","datetime"],
+    "useful": ["button.vote-yes > span"],
+    "unuseful": ["button.vote-no > span"],
+    "content": ["div.user-post__text"],
+    "cons": ["div.review-feature__col:has(> div.review-feature__title--negatives) > div.review-feature__item",None, True],
+    "pros": ["div.review-feature__col:has(> div.review-feature__title--positives) > div.review-feature__item",None, True],
+}
+
 
 product_code="123849599"
 #url="https://www.ceneo.pl/123849599" + product_code +"#tab=reviews"
@@ -11,22 +37,15 @@ response = requests.get(url)
 page_dom = BeautifulSoup(response.text, "html.parser")
 opinions = page_dom.select("div.js_product-review")
 all_opinions=[]
-
 for opinion in opinions:
-    single_opinion = {
-        "opinion_id":opinion["data-entry-id"],
-        "author":opinion.select_one("span.user-post__author-name").text.strip(),
-        "recomendation":opinion.select_one("span.user-post__author-recomendation > em").text.strip(),
-        "stars":opinion.select_one("Span.user-post__score-count").text.strip(),
-        "Purchased":opinion.select_one("div.review-pz").text.strip(),
-        "opinion_date":opinion.select_one("span.user-post__published > time:nth-child(1)")["datetime"].strip(),
-        "purchase_date":opinion.select_one("Span.user-post_published > time:nth-child(2)")["datetime"].strip(),
-        "useful":opinion.select_one("button.vote-yes > span").text.strip(),
-        "unuseful":opinion.select_one("button.vote-no > span").text.strip(),
-        "content":opinion.select_one("div.user-post__text").text.strip(),
-        "cons":[cons.text.strip() for cons in opinion.select_one("div.review-feature__title--negatives ~ div.review-feature__item")],
-        "pros":[pros.text.strip() for pros in opinion.select_one("div.review-feature__title--positives ~ div.review-feature__item")],  
-    }
+    single_opinion={}
+    for key, value in selectors.items():
+        single_opinion[key] = get_element(opinion,value)
     all_opinions.append(single_opinion)
+
+    all_opinions.append(single_opinion)
+
+with open(f"./opinions/{product_code}.json","w",encoding="UTF-8") as jf:
+    json.dump(all_opinions,jf, indent=4, ensure_ascii=False)
     
-print(all_opinions)
+print()
